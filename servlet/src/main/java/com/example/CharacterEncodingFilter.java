b79f39bcd4170ee9264453d0c87e4e3fc0bf3150
Gyuchool <balance0591@naver.com>
2022-09-16 23:20:32 +0900

    feat: filter 추가

--- servlet/src/main/java/com/example/CharacterEncodingFilter.java
+++ servlet/src/main/java/com/example/CharacterEncodingFilter.java
@@ -4,6 +4,11 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+import javax.sound.sampled.AudioFormat.Encoding;
+import org.apache.tomcat.util.buf.Utf8Encoder;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
@@ -11,6 +16,7 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setCharacterEncoding("utf-8");
         chain.doFilter(request, response);
     }
 }
